24223e4a283d83c07b4fd85d93f6b1eb3a83fb3c
Zamoram <42392016+Zamoram@users.noreply.github.com>
2018-11-06 14:36:28 -0800

    trying to program random number generator

--- notes/Guessing number.py
+++ notes/Guessing number.py
@@ -1,11 +1,5 @@
 print("Hello World!")
 
-# Random Numbers
-import random  # This should be on line 1
+# Random numbers
+import random # This should be on line 1
 print(random.randint(0, 10))
-
-# Equality Statements
-print(5 > 3)
-print(5 >= 3)
-print(3 == 4)
-print(3 != 4)
