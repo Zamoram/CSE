346e7721c4a8d02bd79e887a30800b0c8eadc18f
Zamoram <42392016+Zamoram@users.noreply.github.com>
2018-11-08 14:36:59 -0800

    Still trying to work the random generator

--- notes/Guessing number.py
+++ notes/Guessing number.py
@@ -1,9 +1,11 @@
+import random  # This should be on line 1
+
 print("Hello World!")
 
 # Random numbers
-import random # This should be on line 1
-print(random.randint(0, 10))
+answer = random.randint(1, 10)
 guesses = 5
 playing = True
-"While guesses > 0 and playing"
-
+wrong = False
+if wrong:
+    print("Incorrect")
