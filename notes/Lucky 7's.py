25afe5995e61f8d5786c193cf610e31d955872c0
Zamoram <42392016+Zamoram@users.noreply.github.com>
2018-11-13 14:35:52 -0800

    Started programming Lucky 7's

--- notes/Lucky 7's.py
+++ notes/Lucky 7's.py
@@ -1,2 +1,8 @@
-mypot=int(input("$15:"))
-diceroll1=ran
+import random
+mymoney = 15
+
+diceroll1 = random.randint(1, 6)
+diceroll2 = random.randint(1, 6)
+answer = diceroll1 + diceroll2
+if answer == 7:
+    print("YOU WIN!!!!!!!!!!!")
