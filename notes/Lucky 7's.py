f1fb6804b488f2b2b922b52330ad2b03f8c8d57d
Zamoram <42392016+Zamoram@users.noreply.github.com>
2018-11-14 14:37:39 -0800

    Finished Lucky 7's

--- notes/Lucky 7's.py
+++ notes/Lucky 7's.py
@@ -1,8 +1,15 @@
 import random
 mymoney = 15
+rounds = 0
 
-diceroll1 = random.randint(1, 6)
-diceroll2 = random.randint(1, 6)
-answer = diceroll1 + diceroll2
-if answer == 7:
-    print("YOU WIN!!!!!!!!!!!")
+while mymoney > 0:
+    diceroll1 = random.randint(1, 6)
+    diceroll2 = random.randint(1, 6)
+    answer = diceroll1 + diceroll2
+    rounds += 1
+    mymoney -= 1
+    if answer == 7:
+        mymoney += 5
+        print("YOU WIN!!!!!!!!!!!")
+    print(mymoney)
+    print("You played %s rounds" % rounds)
