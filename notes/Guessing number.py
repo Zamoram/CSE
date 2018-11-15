7ec9f627fcb30ce47c7f82d8e9d60648e25d3328
Zamoram <42392016+Zamoram@users.noreply.github.com>
2018-11-15 14:35:20 -0800

    Finished up both projects

--- notes/Guessing number.py
+++ notes/Guessing number.py
@@ -1,11 +1,20 @@
 import random  # This should be on line 1
 
-print("Hello World!")
-
-# Random numbers
 answer = random.randint(1, 10)
 guesses = 5
 playing = True
 wrong = False
-if wrong:
-    print("Incorrect")
+while playing and guesses > 0:
+    guess = input("Choose a number from 1 and 10")
+    if int(guess) == int(answer):
+        print("You have won")
+        playing = False
+
+    else:
+        print("Try Again!!!!")
+        guesses -= 1
+        if int(guess) > int(answer):
+            print("Too High")
+        if int(guess) < int(answer):
+            print("Too Low")
+
