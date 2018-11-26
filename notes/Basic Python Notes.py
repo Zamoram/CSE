f7fb466e1041527473069e36b4ea0d819fde63bd
Zamoram <42392016+Zamoram@users.noreply.github.com>
2018-11-26 14:39:43 -0800

    Learned to make lists

--- notes/Basic Python Notes.py
+++ notes/Basic Python Notes.py
@@ -149,3 +149,32 @@ print(3 != 4)
 a = 3  # A is set to 3
 a == 3 # Is a equal to 3?
 """
+print(3 !=4)
+"""
+a = 3 # A is set to 3
+a = 3 # Is a equal to 3?
+"""
+
+# Creating a List
+fruit = ['apples', 'oranges', 'blackberries', 'strawberries',
+         'blueberries', "raspberries", "pineapple", "mango", "coconut"]
+print(fruit)
+
+# Pulling items from a list
+print(fruit[1])
+
+# Getting the length of a list
+print(len(fruit))
+print("The length of the list is %d" % len(fruit))
+
+# Modifying Lists
+fruit[8] = 'Banana'
+print(fruit)
+
+# Looping through Lists
+for item in fruit:
+    print(item)
+
+icecreamflavours = ['chocolate', 'vanilla', 'mint chocolate', 'cookies n cream', 'strawberry', ' cookie dough',]
+
+new_lsit = ["eggs", "cheese", "oranges", "raspberries",]
