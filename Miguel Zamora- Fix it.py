f4cf219dede8ee4addb0831e7d04f1479f34b77e
Zamoram <42392016+Zamoram@users.noreply.github.com>
2018-12-07 14:45:29 -0800

    almost finished fix it

--- Miguel Zamora- Fix it.py
+++ Miguel Zamora- Fix it.py
@@ -1,11 +1,11 @@
-print("How old are you?", end=' ')
+print("How old are you?")
 age = input()
-print("How tall are you?", end=' ')
+print("How tall are you?")
 height = input()
-print("How much do you weigh?", end=' ')
+print("How much do you weigh?")
 weight = input()
 
-print("So, you're %s old, %s tall and %s heavy.") % (age, height, weight)
+print "So, you're %s old, %s tall and %s heavy." % (age, height, weight)
 
 
 print('Let\'s practice everything.')
@@ -21,18 +21,18 @@ and requires an explanation
 \n\t\twhere there is none.
 """
 
-print("--------------)
+print("--------------")
 print(poem)
-print(--------------")
+print("--------------")
 
 
 five = 10 - 2 + 3 - 6
 print("This should be five: %s" % five)
 
-def secret_formula(started)
+def secret_formula(started):
     jelly_beans = started * 500
     jars = jelly_beans / 1000
-    crates = jars  100
+    crates = jars  / 100
     return jelly_beans, jars, crates
 
 
@@ -42,24 +42,24 @@ beans, jars, crates = secret_formula(start_point)
 # remember that this is another way to format a string
 print("With a starting point of: %d" % start_point)
 # it's just like with an f"" string
-print(f"We'd have {beans} beans, {jars} jars, and {crates} crates.")
+print("fWe'd have {beans} beans, {jars} jars, and {crates} crates.")
 
 start_point = start_point / 10
 
 print("We can also do that this way:")
-formula = secret_formula(startpoint)
+formula = secret_formula("startpoint")
 # this is an easy way to apply a list to a format string
 print("We'd have {} beans, {} jars, and {} crates.".format(*formula))
 
 
 
 people = 20
-cates = 30
+cats = 30
 dogs = 15
 
 
 if people < cats:
-    print "Too many cats! The world is doomed!"
+    print("Too many cats! The world is doomed!")
 
 if people < cats:
     print("Not many cats! The world is saved!")
@@ -67,7 +67,7 @@ if people < cats:
 if people < dogs:
     print("The world is drooled on!")
 
-if people > dogs
+if people > dogs:
     print("The world is dry!")
 
 
@@ -76,9 +76,9 @@ dogs += 5
 if people >= dogs:
     print("People are greater than or equal to dogs.")
 
-if people <= dogs
-    print("People are less than or equal to dogs.)
+if people <= dogs:
+    print("People are less than or equal to dogs.")
 
 
-if people = dogs:
+if people == dogs:
     print("People are dogs.")
--- notes/PythonChallenges.py
+++ notes/PythonChallenges.py
@@ -1,12 +1,21 @@
 # Challenge 1
-def challenge1 (firstname, lastname):
+def challenge1(firstname, lastname):
     print(lastname, firstname)
-
 challenge1("Miguel", "Zamora")
+
+
+# Challenge 4
+def challenge4(number):
+    if number < 0:
+        return"negative"
+    if number > 0:
+        return"positive"
+    if number == 0:
+        return"Zero"
+    print(challenge4(5))
+
 
 # Challenge 3
 def challenge3(b, h):
     return 5 * b * h
 print(challenge3(3, 6))
-
-# Challenge 4
