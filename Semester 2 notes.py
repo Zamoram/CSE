650ba180e41d5580ea314592e1d2fe0242497d55
Miguel Zamora Ramirez 8736 <4sgp@fresnou.org>
2019-01-08 14:38:48 -0800

    reviewed on semester 2 notes

---
+++ Semester 2 notes.py
@@ -0,0 +1,17 @@
+print("Hello World")
+
+# This is a single line comment I think.
+
+cars = 5
+driving = True
+
+print("I have %d cars." % cars)
+print("I have " + str(cars) + "cars.")
+
+age = input("How old are you?")
+print("%s?? Really???" % age)
+
+colors = ["red", "orange", "green", "yellow", "blue"]
+colors.append("cyan")
+
+print(colors)
