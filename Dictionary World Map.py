6a99f72469db887d1a7df13db795854c76027731
Miguel Zamora Ramirez 8736 <4sgp@fresnou.org>
2019-02-07 14:36:17 -0800

    started to do code some of the rooms.

---
+++ Dictionary World Map.py
@@ -0,0 +1,32 @@
+world_map = {
+    'Living Room': {
+        'Name': "Miguels Living Room",
+        'Description': "This is where you live and start.",
+        'Paths': {
+            'North': "Outside",
+            'South': "Dining Room"
+        }
+    },
+    'Dining Room': {
+        'Name': "Eating Area",
+        'Description': "This is where you eat",
+        'Paths': {
+            'East': "Bed_Room",
+            'South': " Kitchen"
+        }
+    },
+    'Outside': {
+        'Name': "Outside of the house",
+        'Description': "You are outside of the house and you can not go any farther and you must go back.",
+        'Paths': {
+            'South': "Living Room"
+        }
+    },
+    'Bed_Room': {
+        'Name': "Place to sleep",
+        'Description': "This is where you sleep and do your homework and where you play video games."
+        'Paths':
+    }
+
+
+}
