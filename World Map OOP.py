d08c67204eb6043a1b34ba3ac02946147119623b
Miguel Zamora Ramirez 8736 <4sgp@fresnou.org>
2019-02-21 14:37:24 -0800

    started doing my world map oop and actually understand it a bit more.

--- World Map OOP.py
+++ World Map OOP.py
@@ -1,21 +1,29 @@
 class Room(object):
     # This is a constructor
-    def __init__(self, name, north=None, south=None, east=None):
+    def __init__(self, name, north=None, south=None, east=None, west=None, description=""):
         self.name = name
         self.north = north
         self.south = south
         self.east = east
+        self.west = west
+        self.description = description
 
 # These are the instances of the rooms (Instantiation)
 
 # Option 1 - Use the variables, but fix later
-R19A = Room("Mr. Wiebe's Room",)
-parking_lot = Room("The Parking Lot", None, R19A)
+living_room = Room("Living Room", None, None, None, None, "This is where you live and start.")
+dining_room = Room("Eating Area", None, None, None, None, "This is where you eat.")
+outside = Room("Outside of the house",None, None,"This is where you sleep and do your homework and where you play video games." )
+kitchen = Room()
+bedroom1 = Room("Place to sleep", None, None, None, None,)
+restroom = Room
 
-R19A.north = parking_lot
+# Fixes
+living_room.north = outside
+living_room.south = dining_room
+dining_room.south = kitchen
+dining_room.east = bedroom1
+outside.south = living_room
 
-# Option 2 - Use strings, but more difficult controller
-R19A = Room("Mr. Wiebe's Room",)
-parking_lot = Room("The Parking Lot", None, R19A)
-
-Living_Room =  Room("")
+bedroom1.north = restroom
+bedroom1.west = dining_room
