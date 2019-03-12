19dd21707a6f7501af0e56e8a0618663ae76bbd4
Miguel Zamora Ramirez 8736 <4sgp@fresnou.org>
2019-03-12 14:36:55 -0700

    coded some more code

--- Items.py
+++ Items.py
@@ -5,7 +5,25 @@ class Item (object):
 
 class Cleaning_supplies(Item):
     def __init__ (self, name):
-        super(Cleaning_supplies, self)
+        super(Cleaning_supplies, self).__init__(name)
         self.cleaning = True
+
+
+class Clorox(Cleaning_supplies):
+    def __init__(self, name):
+        super(Clorox, self).__init__(name)
+        self.grab = True
+
+
+class Broom(Cleaning_supplies):
+    def __init__(self, name):
+        super(Broom, self).__init__(name)
+
+
+class Mop(Cleaning_supplies):
+    def __init__(self,name):
+        super(Mop)
+
+
 
 
