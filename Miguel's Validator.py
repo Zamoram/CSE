5520df618c59c0e9d1a85cb266397d251824baf5
Zamoram <42392016+Zamoram@users.noreply.github.com>
2019-05-02 14:25:54 -0700

    added more code finallt made the code reverse

--- Miguel's Validator.py
+++ Miguel's Validator.py
@@ -1,9 +1,17 @@
 test_num = "6151359320238050"
 
+
 def validate(num: str):
-#Use this for odd numbers    for index in range(len(num)):
-#Use this for odd numbers        int_version = int(num[index])
-        pass
+    for index in range(len(My_List)):
+        My_List[index] = int(My_List[index])
+        for index in range(len(num)):  # This is for the odd numbered positions but it needs work.
+            test_num = int(num[index])
+
+
+My_List = list(test_num)
+print(My_List)
+print(len(My_List))
+
 
 print(validate(test_num))
 num_list = list(test_num)
@@ -11,8 +19,15 @@ num_list.pop(15)
 print(num_list)
 
 
-def reverse_test_num(string):
-    return string[15:0]
-    
+def remember_last_digit(num: str):
+    last_num = int(num[0])  # This needs work on, this is to remember the last digit.
+    if last_num % 2 == 0:
+        return True
+    return False
 
-print(reverse_test_num("6151359320238050"))
+
+def reverse_it(test_num):
+    return test_num[::-1]
+
+
+print(reverse_it("615135932023805"))
