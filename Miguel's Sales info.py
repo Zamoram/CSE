2522fb07a839984af6aecf27bdf181ca345d9a6c
Zamoram <42392016+Zamoram@users.noreply.github.com>
2019-05-13 14:39:01 -0700

    asked help to make another item type was able to get 3 more

--- Miguel's Sales info.py
+++ Miguel's Sales info.py
@@ -2,11 +2,23 @@ import csv
 
 with open("Sales Records.csv", 'r') as csv_file_thing:
     reader = csv.reader(csv_file_thing)
-    total = 0
+    fruits_total = 0
+    meat_total = 0
+    clothes_total = 0
+    beverages_total = 0
+    office_supplies_total = 0
     for row in reader:
         profits = row[13]
         item_type = row[2]
-        if item_type == "Fruits" and "Meat":
-            print(item_type, profits)
-            total += float(profits)
-    print(total)
+        if item_type == "Fruits":
+            fruits_total += float(profits)
+        if item_type == "Meat":
+            meat_total += float(profits)
+        if item_type == "Clothes":
+            clothes_total += float(profits)
+        if item_type == "Beverages":
+            beverages_total += float(profits)
+    print("Fruits", fruits_total)
+    print("Meat", meat_total)
+    print("Clothes", clothes_total)
+    print("Beverages", beverages_total)
